1b01b20ccd679c326aaf03be0a1742b58aec783d
Diego Fernández Barrera <bigomby@gmail.com>
2016-10-06 18:04:17 +0200

    :package: Refactor code

--- src/main.ino
+++ src/main.ino
@@ -1,67 +1,58 @@
-#include "RF24.h"
-#include "RF24Mesh.h"
-#include "RF24Network.h"
 #include <EEPROM.h>
+#include <RF24.h>
+#include <RF24Mesh.h>
+#include <RF24Network.h>
 #include <SPI.h>
 
-RF24 radio(9, 10);
-RF24Network network(radio);
-RF24Mesh mesh(radio, network);
+// Iniciación de librerías
+RF24 radio(9, 10);             // Pines para CE y CSN
+RF24Network network(radio);    // network implementa la capa de red
+RF24Mesh mesh(radio, network); // mesh implementa una red mesh
 
-#define nodeID 1
-#define INTERVAL_MS 10000
+// Configuración
+const uint8_t node_id = 1;         // ID del nodo, 0 es para el master
+const uint32_t interval_ms = 2000; // Intervalo entre envío de mensajes
 
-struct payload_t {
-  unsigned long ms;
-  unsigned long counter;
-};
-
-uint32_t timer = 0;
+// Globales
+uint32_t timer = 0; // Timer usado para los intervalos
 
 void setup() {
-
+  // Se inicia la comunicación con el puerto serie usado para imprimir
+  // información de depuración
   Serial.begin(115200);
 
-  // Set the nodeID manually
-  mesh.setNodeID(nodeID);
+  // Se ajusta la ID del nodo
+  mesh.setNodeID(node_id);
 
-  // Connect to the mesh
-  Serial.println(F("Connecting to the mesh..."));
+  // Se conecta a la red
+  Serial.println(F("Conectando a la red..."));
   mesh.begin();
-
-  // Initialize seed
-  randomSeed(analogRead(0));
 }
 
 void loop() {
-
+  // Hay que llamar periódicamente a esta función
   mesh.update();
 
-  // Send to the master node every second
-  if (millis() - timer >= INTERVAL_MS) {
+  // Se comprueba que haya pasado "interval_ms" milisegundos desde que mandamos
+  // el mensaje anterior
+  if (millis() - timer >= interval_ms) {
+    // Ajustamos los parámetros del mensaje
+    const int message_len = 16;
+    const char message_type = 'R';
+    const char message[message_len] = "hola mundo";
+
+    // Guardamos el instante actual de tiempo
     timer = millis();
 
-    uint32_t randNumber = random(5) + 80;
+    // Se envía el mensaje
+    if (!mesh.write(message, message_type, message_len)) {
 
-    // Send an 'M' type message containing the current millis()
-    if (!mesh.write(&randNumber, 'M', sizeof(randNumber))) {
-
-      // If a write fails, check connectivity to the mesh network
+      // Si falla el envío se comprueba si hay conexión a la red para
+      // volver a conectarse
       if (!mesh.checkConnection()) {
-        // refresh the network address
         Serial.println("Renewing Address");
         mesh.renewAddress();
-      } else {
-        Serial.println("Send fail, Test OK");
       }
-    } else {
-      Serial.print("Send OK: ");
     }
-  }
-
-  while (network.available()) {
-    RF24NetworkHeader header;
-    payload_t payload;
-    network.read(header, &payload, sizeof(payload));
   }
 }
